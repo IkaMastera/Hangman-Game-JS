07ec75cf24f217acc4e5c91f4064549ca44490db
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-03-05 17:27:46 +0400

    add: check function if the letter matches the word

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+const hangmanImage = document.querySelector(".hangman-box img");
 const keyboardDiv = document.querySelector(".keyboard");
 const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text");
+
+let currentWord,
+  correctLetters = [],
+  wrongGuessCount = 0;
+const maxGuesses = 6;
 
 const getRandomWord = () => {
   const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
   console.log(word);
+  currentWord = word;
   document.querySelector(".hint-text b").innerText = hint;
   wordDisplay.innerHTML = word
     .split("")
@@ -11,11 +19,35 @@ const getRandomWord = () => {
     .join("");
 };
 
+const initGame = (button, clickedLetter) => {
+  if (currentWord.includes(clickedLetter)) {
+    [...currentWord].forEach((letter, index) => {
+      if (letter === clickedLetter) {
+        correctLetters.push(letter);
+        wordDisplay.querySelectorAll("li")[index].innerText = letter;
+        wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+      }
+    });
+  } else {
+    wrongGuessCount++;
+    hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+  }
+
+  button.disabled = true;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+  if (wrongGuessCount === maxGuesses) return gameOver(false);
+  if (correctLetters.length === currentWord.length) return gameOver(true);
+};
+
 // Create Keyboard Buttons Dynamically
 for (let i = 97; i < 122; i++) {
   const button = document.createElement("button");
   button.innerText = String.fromCharCode(i);
   keyboardDiv.appendChild(button);
+  button.addEventListener("click", (e) =>
+    initGame(e.target, String.fromCharCode(i))
+  );
 }
 
 getRandomWord();

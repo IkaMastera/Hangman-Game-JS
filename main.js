743424f9d1ab09defe8f7a06e0c5d27b7dd6a15e
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-03-05 17:11:55 +0400

    add: wordlist data, getRandomWord funciton
    added get RandomWord function to get random words from world-list.js data and add them into lists

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const keyboardDiv = document.querySelector(".keyboard");
+const wordDisplay = document.querySelector(".word-display");
+
+const getRandomWord = () => {
+  const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+  console.log(word);
+  document.querySelector(".hint-text b").innerText = hint;
+  wordDisplay.innerHTML = word
+    .split("")
+    .map(() => `<li class="letter"></li>`)
+    .join("");
+};
 
 // Create Keyboard Buttons Dynamically
 for (let i = 97; i < 122; i++) {
@@ -6,3 +17,5 @@ for (let i = 97; i < 122; i++) {
   button.innerText = String.fromCharCode(i);
   keyboardDiv.appendChild(button);
 }
+
+getRandomWord();
